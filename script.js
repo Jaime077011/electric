32de7457054bc0abc520fb8688664891e19ad0b3
Gamal Eldien Gamal <101170188+Jaime077011@users.noreply.github.com>
2023-08-30 20:14:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -260,4 +260,9 @@ loop();
 loop();
 setInterval(loop, 1000 / 60);
 };
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
 
